e64c80b8d0ba88124f92d95d6677f9c088a7310a
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-25 17:03:44 -0700

    Link to /sign-up-form.

--- src/landing-page/sign-up-button.js
+++ src/landing-page/sign-up-button.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function SignUpButton(){
     return(
-        <button>Sign Up</button>
+        <button><Link to='/sign-up-form'>Sign Up</Link></button>
     );
 }
