5ebe8527090bc9f207780fdc4f60fe93f26e2988
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-26 20:18:25 -0700

    Add rules for React.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": [
+        "airbnb",
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    },
+    "plugins": [
+        "react",
+         "jsx-a11y",
+         "babel"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    }
 };
