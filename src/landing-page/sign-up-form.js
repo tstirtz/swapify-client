794753a7b89387536c0c493883b01faa47e0f48c
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-29 21:16:31 -0700

    Make fetch request to POST new user.

--- src/landing-page/sign-up-form.js
+++ src/landing-page/sign-up-form.js
@@ -1,29 +1,98 @@
 import React from 'react';
 import './sign-up-form.css';
 
+const {API_BASE_URL} = require('./../config');
 
-export default function SignUpForm(){
+
+export default class SignUpForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: '',
+      password: '',
+      error: '',
+    }
+  }
+  signUp() {
+    console.log('signUp function called');
+    const newUser ={
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+    }
+    console.log(JSON.stringify(newUser));
+    fetch(`${API_BASE_URL}/sign-up`, {
+      body: JSON.stringify(newUser),
+      cache: 'default',
+      // credentials: 'include',
+      headers: {
+        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      referrer: 'no-referrer',
+    }).then(response => response.json())
+    .then((res) => {
+      // if(!res.ok){
+      //   return Promise.reject(new Error(res.statusText));
+      // }
+      console.log(res);
+      return res;
+    }).then(user => {
+      return Promise.resolve(console.log(user))
+    }).catch(err => console.log(err))
+}
+
+  handleFirstNameChange(event){
+    this.setState({firstName: event.target.value});
+  }
+
+  handleLastNameChange(event){
+    this.setState({lastName: event.target.value});
+  }
+
+  handleEmailChange(event){
+    this.setState({email: event.target.value});
+  }
+
+  handleUsernameChange(event){
+    this.setState({username: event.target.value});
+  }
+
+  handlePasswordChange(event){
+    this.setState({password: event.target.value});
+  }
+
+  render() {
     return(
-        <form id='sign-up-form'>
-            <label for='firstName'>First Name</label>
-            <input id= 'firstName'/>
+      <form id='sign-up-form' onSubmit={e => {e.preventDefault(); this.signUp()}} >
+        <label htmlFor='firstName'>First Name</label>
+        <input id='firstName' value={this.state.firstName} onChange={e => this.handleFirstNameChange(e)} />
 
-            <label for='lastName'>Last Name</label>
-            <input id= 'lastName'/>
+        <label htmlFor='lastName'>Last Name</label>
+        <input id='lastName' value={this.state.lastName} onChange={e => this.handleLastNameChange(e)} />
 
-            <label for='email'>Email Address</label>
-            <input id= 'email'/>
+        <label htmlFor='email'>Email Address</label>
+        <input id='email' value={this.state.email} onChange={e => this.handleEmailChange(e)} />
 
-            <label for='userName'>Username</label>
-            <input id= 'userName'/>
+        <label htmlFor='userName'>Username</label>
+        <input id='userName' value={this.state.username} onChange={e => this.handleUsernameChange(e)} />
 
-            <label for='password'>Password</label>
-            <input id= 'password'/>
+        <label htmlFor='password'>Password</label>
+        <input id='password' value={this.state.password} onChange={e => this.handlePasswordChange(e)} />
 
-            <label for='confirmPassword'>Confirm Password</label>
-            <input id= 'confirmPassword'/>
+        <label htmlFor='confirmPassword'>Confirm Password</label>
+        <input id='confirmPassword' />
 
-            <button type='button' for='sign-up-form'>Submit</button>
-        </form>
+        <button type='submit' htmlFor='sign-up-form'>Submit</button>
+      </form>
     );
+  }
 }
