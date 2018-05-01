336fe6f6108145c309e8663cdc5c7a9b21fcd3bf
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-01 10:50:05 -0700

    Change path of store import.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { store } from './landing-page/redux-actions.test'
+import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
