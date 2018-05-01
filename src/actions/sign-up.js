067bc4cacfd240bcd4e49acb9b8e1258b629382f
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-01 10:53:55 -0700

    Move action adn reducer to separate files.

---
+++ src/actions/sign-up.js
@@ -0,0 +1,56 @@
+import promiseMiddleware from 'redux-promise-middleware';
+const {API_BASE_URL} = require('./../config');
+
+export const SIGN_UP = 'SIGN_UP';
+export function signUp(user){
+  return {
+    type: SIGN_UP,
+    async payload(){
+      let response = await fetch(`${API_BASE_URL}/sign-up`, {
+        body: JSON.stringify(user),
+        // cache: 'default',
+        // credentials: 'include',
+        headers: {
+          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+          'content-type': 'application/json',
+        },
+        method: 'POST',
+        mode: 'cors',
+        redirect: 'follow',
+        // referrer: 'no-referrer',
+      })
+      let data = await response.json();
+      return data;
+    }
+  }
+}
+
+export const SET_FIRST_NAME = 'SET_FIRST_NAME';
+export const setFirstName = value => ({
+  type: SET_FIRST_NAME,
+  value,
+});
+
+export const SET_LAST_NAME = 'SET_LAST_NAME';
+export const setLastName = value => ({
+  type: SET_LAST_NAME,
+  value,
+});
+
+export const SET_EMAIL = 'SET_EMAIL';
+export const setEmail = value => ({
+  type: SET_EMAIL,
+  value,
+});
+
+export const SET_USERNAME = 'SET_USERNAME';
+export const setUsername = value => ({
+  type: SET_USERNAME,
+  value,
+});
+
+export const SET_PASSWORD = 'SET_PASSWORD';
+export const setPassword = value => ({
+  type: SET_PASSWORD,
+  value
+});
---
+++ src/reducers/sign-up-reducer.js
@@ -0,0 +1,60 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import { SET_FIRST_NAME, SET_LAST_NAME, SET_EMAIL, SET_USERNAME, SET_PASSWORD } from '../actions/sign-up';
+
+const initialState = {
+  user: {
+    first: '',
+    last: '',
+    email: '',
+    username: '',
+    password: '',
+  },
+  pending: false,
+}
+export const userInfoReducer = (state=initialState, action) => {
+  if(action.type === SET_FIRST_NAME){
+    return Object.assign({}, state,
+      {user: {...state.user, first : action.value}
+      });
+  }else if(action.type === SET_LAST_NAME){
+    return Object.assign({}, state,
+      {
+        user: {...state.user, last: action.value}
+      });
+  }else if(action.type === SET_EMAIL){
+    return Object.assign({}, state,
+      {
+        user: {...state.user, email: action.value}
+    });
+  }else if(action.type === SET_USERNAME){
+    return Object.assign({}, state,
+      {
+        user: {...state.user, username: action.value}
+    });
+  }else if(action.type === SET_PASSWORD){
+    return Object.assign({}, state,
+      {
+        user: {...state.user, password: action.value}
+    });
+  }
+  return state;
+}
+
+export function signUpReducer(state=initialState, action){
+  if(action.type === 'SIGN_UP_PENDING'){
+    return Object.assign({}, state, {...state, pending: true});
+  }
+  if(action.type === 'SIGN_UP_FULFILLED'){
+    return Object.assign({}, state, {...state, pending: false});
+  }
+  if (action.type === 'SIGN_UP_REJECTED'){
+    return Object.assign({}, state, {...state, error: 'action.payload'});
+  }
+  return state;
+}
+
+export const store = createStore(combineReducers({
+  userInfo: userInfoReducer,
+  signUp: signUpReducer,
+}), applyMiddleware(promiseMiddleware()));
