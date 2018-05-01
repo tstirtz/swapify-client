ac1d7bebebb1b15de0ca492d785e7514e991e233
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-30 23:12:29 -0700

    Add store as prop of Provider component.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { store } from './landing-page/redux-actions.test'
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
