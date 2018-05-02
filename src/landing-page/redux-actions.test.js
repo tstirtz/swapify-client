d8076b3276516ad2f63c651d9ed02d3df051ec94
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-02 15:18:10 -0700

    Add tests for setFirstName, setLastName, setEmail, and setUsername actions.

--- src/landing-page/redux-actions.test.js
+++ src/landing-page/redux-actions.test.js
@@ -1,83 +1,69 @@
-// import { createStore, applyMiddleware, combineReducers } from 'redux';
-// import promiseMiddleware from 'redux-promise-middleware';
-//
-// const initialState = {
-//   user: {
-//     first: '',
-//     last: '',
-//     email: '',
-//     username: '',
-//     password: '',
-//   },
-//   pending: false,
-// }
-// export const userInfoReducer = (state=initialState, action) => {
-//   if(action.type === SET_FIRST_NAME){
-//     return Object.assign({}, state,
-//       {user: {...state.user, first : action.value}
-//       });
-//   }else if(action.type === SET_LAST_NAME){
-//     return Object.assign({}, state,
-//       {
-//         user: {...state.user, last: action.value}
-//       });
-//   }else if(action.type === SET_EMAIL){
-//     return Object.assign({}, state,
-//       {
-//         user: {...state.user, email: action.value}
-//     });
-//   }else if(action.type === SET_USERNAME){
-//     return Object.assign({}, state,
-//       {
-//         user: {...state.user, username: action.value}
-//     });
-//   }else if(action.type === SET_PASSWORD){
-//     return Object.assign({}, state,
-//       {
-//         user: {...state.user, password: action.value}
-//     });
-//   }
-//   return state;
-// }
-//
-// export function signUpReducer(state=initialState, action){
-//   if(action.type === 'SIGN_UP_PENDING'){
-//     return Object.assign({}, state, {...state, pending: true});
-//   }
-//   if(action.type === 'SIGN_UP_FULFILLED'){
-//     return Object.assign({}, state, {...state, pending: false});
-//   }
-//   if (action.type === 'SIGN_UP_REJECTED'){
-//     return Object.assign({}, state, {...state, error: 'action.payload'});
-//   }
-//   return state;
-// }
-//
-// export const store = createStore(combineReducers({
-//   userInfo: userInfoReducer,
-//   signUp: signUpReducer,
-// }), applyMiddleware(promiseMiddleware()));
-// const store = composeStoreWithMiddleware(combineReducers({
-//   userInfo: userInfoReducer,
-//   signUp: signUpReducer,
-// }));
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import { signUp, setFirstName, setLastName, setEmail, setUsername, setPassword } from '../actions/sign-up-actions';
+import { store } from '../store';
 
-// it('signUp', async () => {
-//   const user = {
-//     name: 'Test',
-//   }
-//
-//   const newUser = signUp(user);
-//   store.dispatch(newUser);
-//
-//   console.log(store.getState());
-//
-//   expect(store.getState().pending).toEqual(true);
-//   await newUser.payload
-//   // expect(store.getState().error).toBeDefined;
-//   expect(store.getState().pending).toEqual(false);
-//   // return newUser.payload.then(() => expect(store.getState().pending).toEqual(false));
-// });
+xit('signUp', async () => {
+  const user = {
+    name: 'Test',
+  }
+
+  const newUser = signUp(user);
+  store.dispatch(newUser);
+
+  console.log(store.getState());
+
+  expect(store.getState().signUp.pending).toEqual(true);
+  await newUser.payload
+  // expect(store.getState().error).toBeDefined;
+  expect(store.getState().signUp.pending).toEqual(false);
+  // return newUser.payload.then(() => expect(store.getState().pending).toEqual(false));
+});
+
+it('Should set the users first name', () => {
+  const user = 'Test';
+
+  const action = setFirstName(user);
+  store.dispatch(action);
+
+  expect(store.getState().userInfo.user.first).toEqual('Test');
+});
+
+it('Should set the users last name', () => {
+  const user = 'Test';
+
+  const action = setLastName(user);
+  store.dispatch(action);
+
+  expect(store.getState().userInfo.user.last).toEqual('Test');
+});
+
+it('Should set the users name', () => {
+  const userEmail = 'test@gmail.com';
+
+  const action = setEmail(userEmail);
+  store.dispatch(action);
+
+  expect(store.getState().userInfo.user.email).toEqual(userEmail);
+});
+
+it('Should set the users username', () => {
+  const username = 'test1234';
+
+  const action = setUsername(username);
+  store.dispatch(action);
+
+  expect(store.getState().userInfo.user.username).toEqual(username);
+});
+
+it('Should set the users password', () => {
+  const password = 'test1234';
+
+  const action = setPassword(password);
+  store.dispatch(action);
+
+  expect(store.getState().userInfo.user.password).toBeTruthy();
+});
 
 // it('Should run signUp_REJECTED on action failure', async () => {
 //   const user = {
@@ -85,10 +71,20 @@
 //   }
 //
 //   const newUser = signUp(user);
+//   newUser.payload = Promise.reject('error');
 //   store.dispatch(newUser);
 //
 //   expect(store.getState().pending).toEqual(true);
 //   await newUser.payload
 //   expect(store.getState().error).toEqual('error');
 //
-// })
+// });
+
+xit("sets fulfilled when promise is resolved", () => {
+  const action = signUp();
+  action.payload = Promise.reject("an error");
+
+  return store.dispatch(action).catch(() => {
+    expect(store.getState().signUp).toEqual("an error");
+  });
+});
