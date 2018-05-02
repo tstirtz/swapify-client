1c87bd00a2729ee216e6f805fbf19371c0de95a0
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-01 20:42:25 -0700

    Add materialUI component.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +12,9 @@ import { store } from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
   );
