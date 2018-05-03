cc469c32fadae38c5402ed95fa7a5a7b9f0f82ad
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-02 20:49:12 -0700

    Add forReducer to store.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
+import { reducer as formReducer } from 'redux-form';
 
 import { userInfoReducer, signUpReducer } from './reducers/sign-up-reducer';
 
 export const store = createStore(combineReducers({
+  form: formReducer,
   userInfo: userInfoReducer,
   signUp: signUpReducer,
 }), applyMiddleware(promiseMiddleware()));
