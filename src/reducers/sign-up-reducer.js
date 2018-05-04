6467a79d91a2b325e9448aba780f46751c328858
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-03 22:56:15 -0700

    Remove unused reducer

--- src/reducers/sign-up-reducer.js
+++ src/reducers/sign-up-reducer.js
@@ -12,49 +12,49 @@ const initialState = {
   },
   pending: false,
 }
-export const userInfoReducer = (state=initialState, action) => {
-  if(action.type === SET_FIRST_NAME){
-    return Object.assign({}, state,
-      {user: {...state.user, first : action.value}
-      });
-  }else if(action.type === SET_LAST_NAME){
-    return Object.assign({}, state,
-      {
-        user: {...state.user, last: action.value}
-      });
-  }else if(action.type === SET_EMAIL){
-    return Object.assign({}, state,
-      {
-        user: {...state.user, email: action.value}
-    });
-  }else if(action.type === SET_USERNAME){
-    return Object.assign({}, state,
-      {
-        user: {...state.user, username: action.value}
-    });
-  }else if(action.type === SET_PASSWORD){
-    return Object.assign({}, state,
-      {
-        user: {...state.user, password: action.value}
-    });
-  }
-  return state;
-}
+// export const userInfoReducer = (state=initialState, action) => {
+//   if(action.type === SET_FIRST_NAME){
+//     return Object.assign({}, state,
+//       {user: {...state.user, first : action.value}
+//       });
+//   }else if(action.type === SET_LAST_NAME){
+//     return Object.assign({}, state,
+//       {
+//         user: {...state.user, last: action.value}
+//       });
+//   }else if(action.type === SET_EMAIL){
+//     return Object.assign({}, state,
+//       {
+//         user: {...state.user, email: action.value}
+//     });
+//   }else if(action.type === SET_USERNAME){
+//     return Object.assign({}, state,
+//       {
+//         user: {...state.user, username: action.value}
+//     });
+//   }else if(action.type === SET_PASSWORD){
+//     return Object.assign({}, state,
+//       {
+//         user: {...state.user, password: action.value}
+//     });
+//   }
+//   return state;
+// }
 
 export function signUpReducer(state=initialState, action){
   if(action.type === 'SIGN_UP_PENDING'){
     return Object.assign({}, state, {...state, pending: true});
   }
   if(action.type === 'SIGN_UP_FULFILLED'){
-    return Object.assign({}, state, {...state, pending: false});
+    return Object.assign({}, state, {...state, pending: false, response: `${action.payload}` });
   }
   if (action.type === 'SIGN_UP_REJECTED'){
-    return Object.assign({}, state, {...state, error: 'action.payload'});
+    return Object.assign({}, state, {...state, error: `${action.payload}`});
   }
   return state;
 }
 
 export const store = createStore(combineReducers({
-  userInfo: userInfoReducer,
+  // userInfo: userInfoReducer,
   signUp: signUpReducer,
 }), applyMiddleware(promiseMiddleware()));
