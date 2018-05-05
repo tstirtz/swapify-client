aaee29a841c22053b7b7bfe7b2c88fc932912794
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-05 14:31:42 -0700

    Add redux-form components to LoginForm component

--- src/landing-page/login-form.js
+++ src/landing-page/login-form.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Field, reduxForm } from 'redux-form';
+import renderTextField from './materialUI-text-field';
+import validate from '../validators';
+import { login } from '../actions/login-action';
+import { store } from '../store';
 
-export default function LoginForm(){
-    return(
-        <form id='loginForm'>
-            <label for='username'>Username</label>
-            <input id='username'/>
+import './sign-up-form.css';
 
-            <label for='password'>Password</label>
-            <input id='password'/>
-            <button type='button' for='loginForm'>Login</button>
-        </form>
-    );
+export class LoginForm extends React.Component {
+  handleLoginSubmit(credentials){
+    const userCredentials = {
+      username: credentials.username,
+      password: credentials.password,
+    }
+    console.log('handleLoginSubmit called');
+    this.props.dispatch(login(userCredentials))
+    .then(res => {
+      console.log(res); console.log(store.getState());
+    });
+
+  }
+  render(){
+      return(
+        <div className="redux-form-container">
+          <h2>Login</h2>
+          <form
+            id='loginForm'
+            onSubmit={this.props.handleSubmit(values => this.handleLoginSubmit(values))}
+          >
+            <Field
+              name="username"
+              component={renderTextField}
+              label="Username"
+            />
+            <Field
+              name="password"
+              component={renderTextField}
+              label="Password"
+            />
+            <RaisedButton
+              className="submit-button"
+              type="submit"
+              htmlFor="loginForm"
+              label="Submit"
+              primary={true}
+              disabled={this.props.pristine || this.props.submitting}
+            />
+          </form>
+        </div>
+      );
+  }
 }
+
+export default reduxForm({
+  form: 'loginForm',
+  validate
+})(LoginForm);
