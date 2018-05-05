dd443ecff7746dc344b67c6e94dd28124452a8dc
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-04 18:46:28 -0700

    Remove unused reducer

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import { reducer as formReducer } from 'redux-form';
 
-import { userInfoReducer, signUpReducer } from './reducers/sign-up-reducer';
+import signUpReducer from './reducers/sign-up-reducer';
 
 export const store = createStore(combineReducers({
   form: formReducer,
