09c77b08d27bbc33c777e722e77817afeca59a37
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-05 14:38:29 -0700

    Add loginReducer to store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import { reducer as formReducer } from 'redux-form';
-
 import signUpReducer from './reducers/sign-up-reducer';
+import loginReducer from './reducers/login-reducer';
 
 export const store = createStore(combineReducers({
   form: formReducer,
   signUp: signUpReducer,
+  login: loginReducer,
 }),applyMiddleware(promiseMiddleware()));
