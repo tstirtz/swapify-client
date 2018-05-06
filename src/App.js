80c8345c8b4504a1b9c78dacd9733e02be264af1
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-05 18:02:48 -0700

    Add route for search page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './dashboard/dashboard';
 import LandingPage from './landing-page/landing-page';
 import SignUpForm from './landing-page/sign-up-form';
 import LoginForm from './landing-page/login-form';
+import SearchPage from './search-page/search-page'
 import './App.css';
 
 export default function App(){
@@ -18,6 +19,7 @@ export default function App(){
             <Route exact path='/dashboard' component={Dashboard} />  {/*need to add variable route*/}
             <Route exact path='/login-form' component={LoginForm} />
             <Route exact path='/sign-up-form' component={SignUpForm} />
+            <Route exact path='/search' component={SearchPage} />
           </main>
         </div>
       </Router>
