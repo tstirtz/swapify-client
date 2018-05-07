e86684f67c6d3de8a0bf4a14b06ee6bd4bb4d1de
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-07 16:49:44 -0700

    Add navReducer to store

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { reducer as formReducer } from 'redux-form';
 import signUpReducer from './reducers/sign-up-reducer';
 import loginReducer from './reducers/login-reducer';
+import navReducer from './reducers/nav-reducer';
 
 export const store = createStore(combineReducers({
   form: formReducer,
   signUp: signUpReducer,
   login: loginReducer,
+  nav: navReducer,
 }),applyMiddleware(promiseMiddleware()));
