a75f1a326e3a78b7d25ca37c06165caa805775aa
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-07 16:47:49 -0700

    Add mui Drawer component as side nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import Dashboard from './dashboard/dashboard';
 import LandingPage from './landing-page/landing-page';
 import SignUpForm from './landing-page/sign-up-form';
 import LoginForm from './landing-page/login-form';
 import SearchPage from './search-page/search-page'
+import { navAction } from './actions/nav-action';
 import './App.css';
 
-export default function App(){
+export class App extends React.Component {
+  handleNav = () => this.props.dispatch(navAction(!this.props.open));
+
+  render() {
     return (
       <Router>
         <div className='app'>
           <AppBar
             title='Swapify'
+            onLeftIconButtonClick={this.handleNav}
           />
+          <Drawer
+            open={this.props.open}
+            docked={false}
+            onRequestChange={() => this.props.dispatch(navAction(!this.props.open))}
+          >
+            <MenuItem onClick={this.handleNav}>My Books</MenuItem>
+          </Drawer>
           <main>
             <Route exact path='/' component={LandingPage} />
             <Route exact path='/dashboard' component={Dashboard} />  {/*need to add variable route*/}
@@ -26,3 +41,12 @@ export default function App(){
       </Router>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return {
+    open: state.nav.open,
+  }
+}
+
+export default connect(mapStateToProps)(App);
