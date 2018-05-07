3bd3a5acc92cc6c1d34a69dfc9f549b2c4d42082
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-06 19:25:15 -0700

    Replace component with materialUI component. File no longer needed.

--- src/landing-page/login-button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export default function LoginButton() {
-  return (
-    <button><Link to='/login-form'>Login</Link></button>
-  );
-}
--- src/landing-page/sign-up-button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
-export default function SignUpButton(){
-    return(
-        <button><Link to='/sign-up-form'>Sign Up</Link></button>
-    );
-}
