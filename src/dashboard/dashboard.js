8eccc3c3f06e7d1003dca96e6bcb2c26bcc65d9f
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 10:19:23 -0700

    Remove unused imports

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
 import { AddBook } from './add-book';
 
 export default function Dashboard(){
