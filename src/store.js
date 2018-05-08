6f6f1a01c3e4ab1246ebcf506c39a84a7e6d9262
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 10:19:23 -0700

    Add aaddNeededBookreducer to createStore

--- src/store.js
+++ src/store.js
@@ -4,10 +4,12 @@ import { reducer as formReducer } from 'redux-form';
 import signUpReducer from './reducers/sign-up-reducer';
 import loginReducer from './reducers/login-reducer';
 import navReducer from './reducers/nav-reducer';
+import addNeededBookReducer from './reducers/needed-books-reducer';
 
 export const store = createStore(combineReducers({
   form: formReducer,
   signUp: signUpReducer,
   login: loginReducer,
   nav: navReducer,
+  neededBook: addNeededBookReducer,
 }),applyMiddleware(promiseMiddleware()));
