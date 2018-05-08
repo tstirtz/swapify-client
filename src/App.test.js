3f2168e838d9e3ab4c6341c73fe679ab10d2a969
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-07 19:22:53 -0700

    Add store Provider and MuiTHemProvider wrappers to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
+  // shallow(
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+  // );
 });
