ec40aa58f3a578024d24174e4d477c9f8531c9be
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 09:47:19 -0700

    Create basic component as placeholder

---
+++ src/dashboard/books-to-swap.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BooksToSwap(){
+  return(
+    <h3>Books to swap</h3>
+  )
+}
---
+++ src/dashboard/needed-books.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NeededBooks(){
+  return(
+    <h3>Needed Books</h3>
+  )
+}
