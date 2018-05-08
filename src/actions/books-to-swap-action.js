33594ac3abf35eadec22edc7ea94d04552c341b8
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 10:19:23 -0700

    Add fetch POST call to create a new book

--- src/actions/books-to-swap-action.js
+++ src/actions/books-to-swap-action.js
@@ -1,6 +1,32 @@
+import { SubmissionError } from 'redux-form';
+import { normalizeResponseError } from './utils';
+import { API_BASE_URL } from '../config';
+
 export const BOOKS_TO_SWAP = 'BOOKS_TO_SWAP';
-export const addNeededBook = values => {
+export const addNeededBook = (values, id) => ({
   type: BOOKS_TO_SWAP,
-  values
-  //fetch to endpoint will go here
-}
+  payload() {
+    return fetch(`${API_BASE_URL}/book-to-swap`, {
+      method: 'POST',
+      body: JSON.stringify({title: values.title, author: values.author, userId: `${localStorage.getItem('userId')}`}),
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+        'content-type': 'application/json',
+      },
+      mode: 'cors',
+    }).then(res => normalizeResponseError(res))
+    .then(res => res.json())
+    .catch(err => {
+      const { code } = err;
+      const message =
+        code === 422
+          ? 'Already exists as a needed book'
+          : 'Unable to create book, please try again';
+        console.log(message);
+      // return Promise.reject(
+      //    new SubmissionError(message)
+      // );
+    });
+  }
+});
