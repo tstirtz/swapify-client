47413fcbc30d16017ef60acd5712b6c8067c8925
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 10:19:23 -0700

    Change rendered component

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { AddBook } from './add-book';
+import BooksToSwap from './books-to-swap';
 
 export default function Dashboard(){
     return(
       <div>
         <h1>This is the Dashboard</h1>
-        <AddBook />
+        <BooksToSwap />
       </div>
     );
 }
