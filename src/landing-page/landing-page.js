6a48df619a259bb277608947f7fc8ce59865c19d
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-07 19:52:59 -0700

    Add class name and position buttons

--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Link} from 'react-router-dom';
+import './landing-page.css';
 
 export default function LandingPage(){
     return(
       <div className="signUp-login-buttons">
         <Link to='/sign-up-form'>
           <RaisedButton
-            label="Sign up"
+            className='sign-up-button button'
+            label='Sign up'
             primary={true}
           />
         </Link>
         <Link to='/login-form'>
           <RaisedButton
-            label="Login"
+            className='login-button button'
+            label='Login'
             primary={true}
           />
         </Link>
