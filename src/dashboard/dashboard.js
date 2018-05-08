052b3286aa31f093a4ff32162ab60c89ee10373f
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-08 10:19:23 -0700

    Redner AddBook component

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import { AddBook } from './add-book';
 
 export default function Dashboard(){
     return(
+      <div>
         <h1>This is the Dashboard</h1>
+        <AddBook />
+      </div>
     );
 }
