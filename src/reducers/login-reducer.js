92e2b0a072eee72bc66fe025955e86ce8333ec9b
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-07 20:49:49 -0700

    Add username to state

--- src/reducers/login-reducer.js
+++ src/reducers/login-reducer.js
@@ -9,7 +9,7 @@ export default function loginReducer(state=initialState, action){
   }
   if (action.type === 'LOGIN_FULFILLED') {
     console.log(action.payload);
-    return Object.assign({}, state, {...state, pending: false, id: `${action.payload.id}`, jwt: `${action.payload.jwt}`, status: `${action.payload.status}`, statusText: `${action.payload.statusText}`});
+    return Object.assign({}, state, {...state, pending: false, id: `${action.payload.id}`, jwt: `${action.payload.jwt}`, status: `${action.payload.status}`, statusText: `${action.payload.statusText}`, username:`${action.payload.username}`});
   }
   if (action.type === 'LOGIN_REJECTED') {
     return Object.assign({}, state, {...state, error: `${action.payload.message}`})
