ac919a9db61dec08dc5ddb73a4845f7f84fdf004
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-14 22:12:30 -0700

    Connect to redux store

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,45 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {List, ListItem} from 'material-ui/List';
 import { cyan500 } from 'material-ui/styles/colors';
 import Divider from 'material-ui/Divider';
 import FontIcon from 'material-ui/FontIcon';
 import BooksToSwap from './books-to-swap';
-import { API_BASE_URL } from '../config';
+import { getUserBooks } from '../actions/get-user-books-action';
 
 import './books-to-swap.css';
 
-export default class Dashboard extends React.Component{
+export class Dashboard extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      books: [],
       addBookForm: false,
     };
 
     this.renderForm = this.renderForm.bind(this);
   }
   componentDidMount() {
-    return fetch(`${API_BASE_URL}/user-books/${localStorage.getItem('userId')}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
-        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-      },
-      mode: 'cors',
-    })
-    .then(res => res.json())
-    .then(jsonData => {
-      console.log(jsonData);
-      jsonData.map(book => this.setState({ books: [...this.state.books, book] }));
-    })
-    .catch(err => {
-      console.log(err);
-      const message = "Couldn't retrieve data";
-      return message;
-    })
+    return this.props.dispatch(getUserBooks());
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props.books !== nextProps.books ||
+    this.state.addBookForm !== nextState.addBookForm;
   }
   renderForm(){
-    this.setState({addBookForm: !this.state.addBookForm})
+    console.log('renderForm called');
+    this.setState({addBookForm: !this.state.addBookForm })
   }
   render(){
     return(
@@ -50,7 +38,11 @@ export default class Dashboard extends React.Component{
           >
             My Books
           </h2>
-          {this.state.addBookForm && <BooksToSwap addBookForm={() => this.renderForm} />}
+          {this.state.addBookForm &&
+            <BooksToSwap
+              toggleBookForm={this.renderForm}
+              addBookFormState={this.state.addBookForm}
+            />}
           <FontIcon
             onClick={this.renderForm}
             className='fas fa-plus render-form-button'
@@ -58,9 +50,8 @@ export default class Dashboard extends React.Component{
             style={{fontSize: "18px"}}
           />
         </div>
-        {/* {this.state.addBookForm && <BooksToSwap />} */}
         <List>
-          {this.state.books.map( book => {
+          {this.props.books.map( book => {
             return(
               <div
                 key={book._id}
@@ -83,3 +74,14 @@ export default class Dashboard extends React.Component{
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.userBooks.books,
+    error: state.userBooks.error,
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(Dashboard);
