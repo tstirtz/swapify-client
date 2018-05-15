eeb1c087dc2b47ee09b231be1e2283c81c0ade76
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-15 12:01:06 -0700

    Change pending state for FULFILLED action

--- src/reducers/get-user-books-reducer.js
+++ src/reducers/get-user-books-reducer.js
@@ -8,7 +8,7 @@ export default function getUserBooksReducer(state=initialState, action){
     return Object.assign({}, state, {...state, pending: true})
   }
   if(action.type === 'GET_USER_BOOKS_FULFILLED'){
-    return Object.assign({}, state, {...state, books: action.payload, success: true})
+    return Object.assign({}, state, {...state, pending: false, books: action.payload, success: true})
   }
   if(action.type === 'GET_USER_BOOKS_REJECTED'){
     return Object.assign({}, state, {...state, error: `${action.payload}`, success: false},)
