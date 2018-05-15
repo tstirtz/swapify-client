676754939b553af29ebf9b16b3275612f844d794
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-15 12:01:52 -0700

    Add components to render search results and error message

--- src/search-page/search-page.js
+++ src/search-page/search-page.js
@@ -1,16 +1,67 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import FontIcon from 'material-ui/FontIcon';
+import { cyan500 } from 'material-ui/styles/colors';
+import { getAllBooks } from '../actions/get-all-books-action';
 
 import './search-page.css';
 
-export default function SearchPage(){
-  return(
-    <div className="search-field">
-      <TextField
-        hintText="Search"
-        type="search"
-        fullWidth={true}
-      /><br />
-    </div>
-  )
+export class SearchPage extends React.Component{
+  componentDidMount(){
+    this.props.dispatch(getAllBooks());
+  }
+  render(){
+    const errorCode = this.props.books.code;
+    const errorMessage = this.props.books.message;
+    const list = this.props.books.code ? (
+      <p className= "error-message">{`${errorCode} ${errorMessage}`}</p>
+    ) : (
+      <List className="search-results">
+        {
+          this.props.books.map(book => {
+            return (
+              <div
+                key={book._id}
+              >
+                <ListItem
+                  leftIcon={<FontIcon
+                    className="fas fa-book"
+                    color={cyan500}
+                  />}
+                  primaryText={book.title}
+                  secondaryText={book.author}
+                />
+                <Divider />
+              </div>
+            )
+          })
+        }
+      </List>
+    )
+    return(
+      <div>
+        <div className="search-field">
+          <TextField
+            hintText="Search"
+            type="search"
+            fullWidth={true}
+          /><br />
+        </div>
+        {list}
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state){
+  return {
+    books: state.allBooks.books,
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(SearchPage);
