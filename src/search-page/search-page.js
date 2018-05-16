23441d51ce5d774f4d85d9dc7ebf97148f9428b6
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-15 21:34:41 -0700

    Add function to filter results based on search value

--- src/search-page/search-page.js
+++ src/search-page/search-page.js
@@ -10,10 +10,30 @@ import { getAllBooks } from '../actions/get-all-books-action';
 import './search-page.css';
 
 export class SearchPage extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      // books: this.props.books,
+      search: ''
+    }
+  }
   componentDidMount(){
     this.props.dispatch(getAllBooks());
   }
+  // filterResults(value){
+  //   return this.props.books.filter(book => {
+  //     return book.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+  //   });
+  // }
+  updateSearch(event){
+    this.setState({search: event.target.value})
+  }
   render(){
+    console.log(this.props.books);
+    let filteredBooks = this.props.books.filter((book) => {
+      return book.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    });
+
     const errorCode = this.props.books.code;
     const errorMessage = this.props.books.message;
     const list = this.props.books.code ? (
@@ -21,7 +41,7 @@ export class SearchPage extends React.Component{
     ) : (
       <List className="search-results">
         {
-          this.props.books.map(book => {
+          filteredBooks.map(book => {
             return (
               <div
                 key={book._id}
@@ -45,6 +65,8 @@ export class SearchPage extends React.Component{
       <div>
         <div className="search-field">
           <TextField
+            onChange={this.updateSearch.bind(this)}
+            value={this.state.search}
             hintText="Search"
             type="search"
             fullWidth={true}
