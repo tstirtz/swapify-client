85df40e9934a5ac09c81fbd846c8b1a6b0f30dab
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-16 21:08:39 -0700

    Create a modal to send messages to the owner of a book

---
+++ src/search-page/message-modal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
+export default class MessageModal extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.close = this.close.bind(this);
+  }
+  close(){
+    this.props.closeModal();
+  }
+  render() {
+    const actions = [
+      <FlatButton
+        label='Cancel'
+        primary={true}
+      />,
+      <FlatButton
+        label='Send'
+        primary={true}
+      />
+    ];
+    return (
+      <div>
+        <Dialog
+          title="Message Owner"
+          titleStyle={{
+            fontSize: "18px",
+            width: "80%",
+            textAlign: "center",
+            marginRight: "10%",
+            marginLeft: "10%",
+          }}
+          actions={actions}
+          open={this.props.modalState}
+          onRequestClose={this.close}
+        >
+          <TextField
+            name="message-field"
+            fullWidth={true}
+            multiLine={true}
+            inputStyle={{
+              height: "60%",
+            }}
+          />
+        </Dialog>
+      </div>
+    );
+  }
+}
