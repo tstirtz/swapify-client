7535e638926539f2d85e7b94ed1d256eec728592
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-19 15:08:13 -0700

    Create async reducer

---
+++ src/reducers/send-message-reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  status: '',
+}
+
+export default function sendMessageReducer(state=initialState, action){
+  if(action.type === 'SEND_MESSAGE_PENDING'){
+    return Object.assign({}, state, {...state, pending: true});
+  }
+  if(action.type === 'SEND_MESSAGE_FULFILLED'){
+    return Object.assign({}, state, {...state, state: action.payload});
+  }
+  if(action.type === 'SEND_MESSAGE_REJECTED'){
+    return Object.assign({}, state, {...state, error: action.payload});
+  }
+  return state;
+}
