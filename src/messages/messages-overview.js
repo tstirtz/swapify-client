722095c1e929a584aaad3d612afe4223496daaa5
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-20 11:47:31 -0700

    Dispatch action to retrieve messages

--- src/messages/messages-overview.js
+++ src/messages/messages-overview.js
@@ -1,42 +1,47 @@
 import React from 'react';
-import faker from 'faker';
+import { connect } from 'react-redux';
 import {List, ListItem} from 'material-ui/List';
 import FontIcon from 'material-ui/FontIcon';
 import Divider from 'material-ui/Divider';
 import { Link } from 'react-router-dom';
+import { getMessages } from '../actions/get-messages-action';
 // import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 // import Avatar from '@material-ui/core/Avatar';
 // import People from '@material-ui/icons/People';
 
 import './messages.css';
 
-export default class MessagesOverview extends React.Component{
+export class MessagesOverview extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      messages: [
-        {
-          from: 'jimBo123',
-          timeStamp: faker.date.past,
-          _id: 1,
-        },
-        {
-          from: 'masterChief117',
-          timeStamp: faker.date.past,
-          _id: 2,
-        },
-        {
-          from: 'bigFudge97',
-          timeStamp: faker.date.past,
-          _id: 3,
-        },
-      ]
-    }
+    //   messages: [
+    //     {
+    //       from: 'jimBo123',
+    //       timeStamp: faker.date.past,
+    //       _id: 1,
+    //     },
+    //     {
+    //       from: 'masterChief117',
+    //       timeStamp: faker.date.past,
+    //       _id: 2,
+    //     },
+    //     {
+    //       from: 'bigFudge97',
+    //       timeStamp: faker.date.past,
+    //       _id: 3,
+    //     },
+    //   ]
+     }
+  }
+  componentDidMount(){
+    console.log('componentDidMount called');
+    this.props.dispatch(getMessages());
   }
   render(){
     return(
       <List>
-        {this.state.messages.map( message => {
+        {this.props.messages.map( message => {
           return(
             <div key={message._id}>
               <Link to={`/${localStorage.getItem('userId')}/message-thread`}>
@@ -60,3 +65,11 @@ export default class MessagesOverview extends React.Component{
     );
   }
 }
+
+function mapStateToProps(state){
+  return{
+    messages: state.getMessages.messages
+  }
+}
+
+export default connect(mapStateToProps)(MessagesOverview);
