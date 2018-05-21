07c53e29ca44d8d5997f034fd2a0a75162f3f22e
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-20 17:43:11 -0700

    Fix bug to successfully render and close snackbar

--- src/messages/message-thread.js
+++ src/messages/message-thread.js
@@ -2,22 +2,58 @@ import React from 'react';
 import { connect } from 'react-redux';
 import IconButton from 'material-ui/IconButton';
 import Textarea from "react-textarea-autosize";
+import Snackbar from 'material-ui/Snackbar';
 import { pinkA200 } from 'material-ui/styles/colors';
 import { getMessages } from '../actions/get-messages-action';
+import { sendMessage } from '../actions/send-message-action';
+
 
 export class MessageThread extends React.Component{
   constructor(props){
     super(props)
     this.state = {
       newMessage: '',
+      snackbarRendered: false,
     }
-    this.handleTextAreaChange =this.handleTextAreaChange.bind(this);
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.openSnackbar = this.openSnackbar.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
+
   componentDidMount(){
     this.props.dispatch(getMessages());
   }
+
   handleTextAreaChange(event){
     this.setState({ newMessage: event.target.value})
+  }
+
+  sendMessage(){
+    let pathname = window.location.pathname;
+    let pathnameValues = pathname.split('/');
+    let recipient = pathnameValues[1];
+
+    this.props.dispatch(sendMessage(this.state.newMessage, recipient))
+      .then(() => {
+        this.openSnackbar();
+        if(this.props.messageStatus === 'Message sent'){
+          this.setState({ newMessage: ''});
+        }
+      });
+  }
+
+  openSnackbar(){
+    console.log('------------- openSnackBar called');
+    this.setState({
+      snackbarRendered: true,
+    })
+  }
+
+  closeSnackbar(){
+    this.setState({
+      snackbarRendered: false,
+    })
   }
   render() {
     let pathname = window.location.pathname
@@ -59,22 +95,21 @@ export class MessageThread extends React.Component{
             value={this.state.newMessage}
             onChange={this.handleTextAreaChange}
           />
-          {/* <TextField
-            name="message-field"
-            hintText="Want to swap?"
-            // fullWidth={true}
-            multiLine={true}
-            inputStyle={{
-              height: "60%",
-            }}
-          /> */}
           <IconButton
             iconClassName="fas fa-arrow-circle-up"
             iconStyle={{
               iconHoverColor:`${pinkA200}`,
             }}
+            onClick={this.sendMessage}
           />
         </div>
+        <Snackbar
+          className="snackbar"
+          open={this.state.snackbarRendered}
+          autoHideDuration={4000}
+          onRequestClose={this.closeSnackBar}
+          message={this.props.messageStatus}
+        />
       </div>
     );
   }
@@ -83,6 +118,7 @@ export class MessageThread extends React.Component{
 function mapStateToProps(state){
   return{
     messages: state.getMessages.messages,
+    messageStatus: state.sendMessage.status,
   }
 }
 
