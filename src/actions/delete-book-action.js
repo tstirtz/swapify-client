54777b7b3ef2e7c7645aa867387a51ff9de981c5
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-21 16:12:05 -0700

    Create action that makes fetch request to api to delete a book

---
+++ src/actions/delete-book-action.js
@@ -0,0 +1,26 @@
+import { API_BASE_URL } from '../config';
+
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const deleteBook = bookId => ({
+  type: DELETE_BOOK,
+  payload(){
+    return fetch(`${API_BASE_URL}/${bookId}/delete-book`,{
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+        'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+        'content-type': 'application/json',
+      }
+    })
+    .then(res => res.json())
+    .then(jsonResponse => {
+        console.log(jsonResponse);
+        return jsonResponse;
+    })
+    .catch(err => {
+      console.log(err);
+      return err;
+    })
+  }
+})
