ca486902faf4abe5691bd805413141ecba762e2f
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-24 07:50:51 -0700

    Comment out unused test

--- src/dashboard/dashboard.test.js
+++ src/dashboard/dashboard.test.js
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -15,4 +15,20 @@ describe('Dashboard page', () => {
         </MuiThemeProvider>
       </Provider>);
   });
+  // it('BookToSwap modal should render when addBookForm state is true', () => {
+  //   const spy = jest.fn();
+  //   const wrapper = shallow(
+  //     <Provider store={store}>
+  //       <MuiThemeProvider>
+  //         <DashBoard />
+  //       </MuiThemeProvider>
+  //     </Provider>);
+  //
+  //   wrapper.instance().renderForm;
+  //   wrapper.update();
+  //
+  //   const modal = wrapper.find('.subheader').dive().find('BooksToSwap');
+  //
+  //   expect(modal.length).toEqual(2);
+  // });
 });
