cb0a64b0454e42e616dc06da566489d98dcd8deb
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-23 22:33:35 -0700

    Add placeholder image

--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -5,21 +5,28 @@ import './landing-page.css';
 
 export default function LandingPage(){
     return(
-      <div className="signUp-login-buttons">
-        <Link to='/sign-up-form'>
-          <RaisedButton
-            className='sign-up-button button'
-            label='Sign up'
-            primary={true}
-          />
-        </Link>
-        <Link to='/login-form'>
-          <RaisedButton
-            className='login-button button'
-            label='Login'
-            primary={true}
-          />
-        </Link>
+      <div className='landing-page-container'>
+        <img
+          src='https://www.colourbox.com/preview/13896125-doodle-recycle-symbol.jpg'
+          alt='three arrows makings a triangle'
+          className='logo'
+        />
+        <div className='signUp-login-buttons'>
+          <Link to='/sign-up-form'>
+            <RaisedButton
+              className='sign-up-button button'
+              label='Sign up'
+              primary={true}
+            />
+          </Link>
+          <Link to='/login-form'>
+            <RaisedButton
+              className='login-button button'
+              label='Login'
+              primary={true}
+            />
+          </Link>
+        </div>
       </div>
     );
 }
