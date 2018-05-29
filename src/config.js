4ae7a7f5e89e34fba676d37674904e8b654b3349
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-29 11:22:33 -0700

    Change export in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  API_BASE_URL: process.env.API_BASE_URL || global._API_BASE_URL || 'http://localhost:8000',
-}
+// module.exports = {
+//   API_BASE_URL: process.env.API_BASE_URL || global._API_BASE_URL || 'http://localhost:8000',
+// }
+exports.API_BASE_URL = process.env.API_BASE_URL || global._API_BASE_URL || 'http://localhost:8000';
