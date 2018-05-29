52dc73bdb0c0625f188b0e5898c55b0e8f3c2b2a
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-29 11:51:37 -0700

    Change env variable name

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // module.exports = {
 //   API_BASE_URL: process.env.API_BASE_URL || global._API_BASE_URL || 'http://localhost:8000',
 // }
-exports.API_BASE_URL = process.env.API_BASE_URL || global._API_BASE_URL || 'http://localhost:8000';
+exports.API_BASE_URL = process.env.REACT_APP_API_BASE_URL || global.REACT_APP_API_BASE_URL || 'http://localhost:8000';
