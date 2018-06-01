e0853813ad324367538aa333fc94fb1079bb14d9
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-05-31 21:59:03 -0700

    Write test for loading indicator

--- src/landing-page/login-form.test.js
+++ src/landing-page/login-form.test.js
@@ -1,18 +1,33 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import { Provider } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LinearProgress from 'material-ui/LinearProgress';
 import store from '../store';
 import { LoginForm } from './login-form';
 
 describe('<LoginForm/>', () => {
+  const render = (props = {}) => {
+    const defaultProps = {openForm: true, closeForm: jest.fn(), pristine: true, submitting: false, handleSubmit:jest.fn() }
+    props = {...defaultProps, ...props};
+    return (
+      <LoginForm {...props}   />
+    )
+  }
   test('Should render without crashing', () => {
     shallow(
       <Provider store={store}>
-        <MuiThemeProvider>
-          <LoginForm />
-        </MuiThemeProvider>
+        {render()}
       </Provider>
     );
   });
+  test('it renders loading indicator when pending', () => {
+      const component = shallow(render({pending: true}));
+
+      expect(component.find(LinearProgress).length).toEqual(1);
+  })
+  test('it does not renders loading indicator', () => {
+      const component = shallow(render({pending: false}));
+
+      expect(component.find(LinearProgress).length).toEqual(0);
+  })
 });
